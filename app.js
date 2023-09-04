0626a897f1c0f67cecb1b7d4f8d4dc082686b50c
BGasser <axel.salgado55@gmail.com>
2023-09-03 18:58:44 -0600

    Added: app.js, package.json

---
+++ app.js
@@ -0,0 +1,21 @@
+import express, { urlencoded } from 'express'
+import { moviesRouter } from './routes/movies.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.use(urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.redirect('/movies')
+})
+
+app.use('/movies', moviesRouter)
+
+app.listen(port, () => {
+  console.log()
+  console.log(`Running on http://localhost:${port}`)
+  console.log()
+})
