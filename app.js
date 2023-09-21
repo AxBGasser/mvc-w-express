9fb54d2bdb0d0bfc1513b57009db9b3c728c48bc
Axel BGasser <axel.salado55@gmail.com>
2023-09-21 16:49:51 +0000

    + Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import express, { urlencoded } from 'express'
+import helmet from 'helmet'
+import nocache from 'nocache'
+import compression from 'compression'
 import { moviesRouter } from './routes/movies.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const app = express()
 const port = process.env.PORT
+
+const app = express()
+app.use(compression())
+app.use(nocache())
+app.use(helmet({
+  xDownloadOptions: false,
+  xPoweredBy: false,
+  strictTransportSecurity: {
+    maxAge: 63072000,
+    preload: true,
+  },
+
+}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
